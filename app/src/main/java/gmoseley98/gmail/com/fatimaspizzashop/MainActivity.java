860ede411b95c1217a7cf3ff7549993553864494
gmoseley1998 <gmoseley98@gmail.com>
2019-01-28 14:23:29 -0500

    first update

--- app/src/main/java/gmoseley98/gmail/com/fatimaspizzashop/MainActivity.java
+++ app/src/main/java/gmoseley98/gmail/com/fatimaspizzashop/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void validate(String userName, String password) {
-        if((userName.equals("admin")) && (password.equals("2019"))) {
+        if((userName.equals("admin")) && (password.equals("1998"))) {
             Toast.makeText(getApplicationContext(),
                     "Redirecting...",Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(MainActivity.this, HomeScreen.class);
