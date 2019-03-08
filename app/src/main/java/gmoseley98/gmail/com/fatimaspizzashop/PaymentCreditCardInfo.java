d0b538019545817e989335ad658fe37c9c121217
Ethan Rimer <erimer9567@g.fmarion.edu>
2019-03-08 14:22:57 -0500

    Payment screens more or less done. Constraints/functionality to be added.

---
+++ app/src/main/java/gmoseley98/gmail/com/fatimaspizzashop/PaymentCreditCardInfo.java
@@ -0,0 +1,48 @@
+/********************************
+ * Author: Ethan Rimer
+ * Class Name: PaymentCreditCardInfo
+ ********************************/
+package gmoseley98.gmail.com.fatimaspizzashop;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class PaymentCreditCardInfo extends AppCompatActivity {
+
+    private EditText cardNumEditText, nameEditText, securityNumEditText,
+            expDateEditText;
+
+    private Button finishBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_credit_card_info);
+
+        cardNumEditText = (EditText) findViewById(R.id.cardNumEditText);
+        nameEditText = (EditText) findViewById(R.id.nameEditText);
+        securityNumEditText = (EditText) findViewById(R.id.securityNumEditText);
+        expDateEditText = (EditText) findViewById(R.id.expDateEditText);
+
+        finishBtn = (Button) findViewById(R.id.finishBtn);
+
+        finishBtn.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                finishOrder();
+            }
+        }));
+    }
+
+    public void finishOrder() {
+
+        Intent intent = new Intent(this, HomeScreen.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/gmoseley98/gmail/com/fatimaspizzashop/PaymentSelectPayment.java
+++ app/src/main/java/gmoseley98/gmail/com/fatimaspizzashop/PaymentSelectPayment.java
@@ -1,8 +1,15 @@
+/********************************
+ * Author: Ethan Rimer
+ * Class Name: PaymentSelectPayment
+ ********************************/
 package gmoseley98.gmail.com.fatimaspizzashop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
@@ -11,7 +18,11 @@ public class PaymentSelectPayment extends AppCompatActivity {
     private RadioGroup paymentTypeRadGroup;
     private RadioButton creditCardRadBtn, cashRadBtn;
 
+    private EditText priceEditText;
+
     private Button nextBtn;
+
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +33,40 @@ public class PaymentSelectPayment extends AppCompatActivity {
         creditCardRadBtn = (RadioButton) findViewById(R.id.creditCardRadBtn);
         cashRadBtn = (RadioButton) findViewById(R.id.cashRadBtn);
 
+        priceEditText = (EditText) findViewById(R.id.priceEditText);
+
         nextBtn = (Button) findViewById(R.id.nextBtn);
+
+        paymentTypeRadGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId == R.id.creditCardRadBtn) {
+
+                    creditCardSelected();
+                }
+                else if(checkedId == R.id.cashRadBtn) {
+
+                    cashSelected();
+                }
+            }
+        });
+
+        nextBtn.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(intent);
+            }
+        }));
+    }
+
+    public void creditCardSelected() {
+
+        intent = new Intent(this, PaymentCreditCardInfo.class);
+    }
+
+    public void cashSelected() {
+
+        intent = new Intent(this, HomeScreen.class);
     }
 }
